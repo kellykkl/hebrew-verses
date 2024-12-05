24660132c67789045fb017f1ad4bf1a089a7eba5
kelly <kellykkl@gmail.com>
2024-12-05 20:38:15 +0800

    increase timeout

--- app.py
+++ app.py
@@ -313,4 +313,4 @@ def process_data():
 
 if __name__ == '__main__':
     from os import environ
-    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)), timeout=120)
